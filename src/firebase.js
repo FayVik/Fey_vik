847efd5a3572d09f770ede226e3f2dadaa8fcec6
FayVik <vivianemma97@gmail.com>
2021-09-25 14:11:57 +0100

    uncomment firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const config = {
-	apiKey: process.env.REACT_APP_FIREBASE_KEY,
-	authDomain: process.env.REACT_APP_FIREBASE_KEY_DOMAIN,
-	projectId: process.env.REACT_APP_FIREBASE_KEY_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_FIREBASE_KEY_STORAGE_BUCKET,
+	apiKey: process.env.API_KEY,
+	authDomain: process.env.AUTH_DOMAIN,
+	projectId: process.env.PID,
+	storageBucket: process.env.BUCKET,
 	messagingSenderId: process.env.REACT_APP_FIREBASE_KEY_SENDER_ID,
 	appId: process.env.REACT_APP_FIREBASE_KEY_APP_ID,
-	measurementId: process.env.REACT_APP_FIREBASE_KEY_MEASURMENT_ID,
+	measurementId: process.env.MSGID,
 };
 
 firebase.initializeApp(config);
